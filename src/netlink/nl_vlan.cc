96be713b932d80edb22188c05cd0b993d5311e4e
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2020-12-01 10:32:25 +0100

    fix trailing type

--- src/netlink/nl_vlan.cc
+++ src/netlink/nl_vlan.cc
@@ -171,7 +171,7 @@ uint16_t nl_vlan::get_vid(rtnl_link *link) {
   return vid;
 }
 
-nl_vlan::get_vrf_id(uint16_t vid) -> uint16_t {
+uint16_t nl_vlan::get_vrf_id(uint16_t vid) {
   auto it_range = vlan_vrf.find(vid);
   if (it_range == vlan_vrf.end()) {
     LOG(ERROR) << __FUNCTION__ << ": did not find correct vrf id for vlan=" << vid;
