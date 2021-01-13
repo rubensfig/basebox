be1cfd07c8737fd2994038bb769a948ad79552de
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2021-01-13 17:32:07 +0100

    debug: vlan message reception

--- src/netlink/cnetlink.cc
+++ src/netlink/cnetlink.cc
@@ -550,6 +550,7 @@ void cnetlink::handle_wakeup(rofl::cthread &thread) {
 #endif
     case RTM_NEWVLAN:
     case RTM_DELVLAN:
+      LOG(INFO) << "RECEIVED MESSAGE";
       route_bridge_vlan_apply(obj);
       break;
     default:
